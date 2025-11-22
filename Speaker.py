ab1257a3bd7d634b572564260432ce2e94c9f216
luis7diaz <mukombatapiwa@gmail.com>
2025-11-22 11:14:34 +0530

    Add files via upload

---
+++ Speaker.py
@@ -0,0 +1,7 @@
+import pyttsx3
+engine=pyttsx3.init()
+msg=input("Enter text to speak::")
+speed=int(input("speed(100-300:)"))
+engine.setProperty("rate",speed)
+engine.say(msg)
+engine.runAndWait()
